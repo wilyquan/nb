64e76dba57cc3bcb7704ca4b128f8790370016a6
jewelvary <jewelvary@jewelvarydembp.lan>
2018-03-09 15:50:35 +0800

    create

---
+++ nb-master/nb-api/src/main/java/com/nb/api/NbApiApplication.java
@@ -0,0 +1,12 @@
+package com.nb.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbApiApplication.class, args);
+	}
+}
