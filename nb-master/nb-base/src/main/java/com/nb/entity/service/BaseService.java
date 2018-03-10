bc5fa68eb1d623a19a3956762b0f4588b276f3d9
willie <willie@192.168.0.177>
2018-03-10 20:15:07 +0800

    update

--- nb-master/nb-base/src/main/java/com/nb/entity/service/BaseService.java
+++ nb-master/nb-base/src/main/java/com/nb/entity/service/BaseService.java
@@ -3,7 +3,6 @@
  */
 package com.nb.entity.service;
 
-import javax.transaction.Transactional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- nb-master/nb-base/src/main/java/com/nb/entity/service/CrudService.java
+++ nb-master/nb-base/src/main/java/com/nb/entity/service/CrudService.java
@@ -4,10 +4,12 @@
 package com.nb.entity.service;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.nb.entity.BaseDao;
 import com.nb.entity.DbEntity;
@@ -56,6 +58,14 @@ public abstract class CrudService<D extends BaseDao<T, ID>, T extends DbEntity, 
 	 */
 	public Iterable<T> findAll() {
 		return dao.findAll();
+	}
+
+	public Iterable<T> findAll(Sort sort) {
+		return dao.findAll(sort);
+	}
+
+	public Page<T> findAll(Pageable pageable) {
+		return dao.findAll(pageable);
 	}
 
 	/**
