6b13b277c7993b114dbc17876f478ca047f47c37
jewelvary <jewelvary@jewelvarydembp.lan>
2018-03-13 09:59:17 +0800

    update actuator

---
+++ nb-master/nb-base/src/main/java/com/nb/endpoint/PersonEndpoint.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package com.nb.endpoint;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author jewelvary
+ *
+ */
+@Endpoint(id = "person")
+@Component
+public class PersonEndpoint {
+
+    private final Map<String, Person> people = new HashMap<>();
+
+    PersonEndpoint() {
+        this.people.put("mike", new Person("Michael Redlich"));
+        this.people.put("rowena", new Person("Rowena Redlich"));
+        this.people.put("barry", new Person("Barry Burd"));
+    }
+
+    @ReadOperation
+    public List<Person> getAll() {
+        return new ArrayList<>(this.people.values());
+    }
+
+    @ReadOperation
+    public Person getPerson(@Selector String person) {
+        return this.people.get(person);
+    }
+
+    @WriteOperation
+    public void updatePerson(@Selector String name, String person) {
+        this.people.put(name, new Person(person));
+    }
+
+    public static class Person {
+        private String name;
+
+        Person(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
