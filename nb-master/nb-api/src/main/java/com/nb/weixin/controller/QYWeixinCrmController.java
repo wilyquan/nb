f6ed7e24e0b26fd14a676388a4fb06a3bbc810bd
jewelvary <jewelvary@192.168.250.135>
2018-03-13 18:10:26 +0800

    update

--- nb-master/nb-api/src/main/java/com/nb/weixin/controller/QYWeixinCrmController.java
+++ nb-master/nb-api/src/main/java/com/nb/weixin/controller/QYWeixinCrmController.java
@@ -42,3 +42,10 @@ public class QYWeixinCrmController extends QYWeixinControllerSupport {
 	}
 
 }
+
+//授权信息
+//
+//<xml><ToUserName><![CDATA[toUser]]></ToUserName><Encrypt><![CDATA[PUFDPq5hSzxDl5XPu1HVtzbAmf7wW8at3l8SBlvcLn/09ZS3YngX+NiCMotaHuJSMiTPzAZbEHx6lfPzcFAXP+ruKts6eqYlyS/yaUBkea7H1ndGGJ3oGdsD8s0lxoxWZ3bPf4BZCyDdT75Gh5mW4I/x6Y1E3X0Swct8lN1LV35KNXAVkwz/mngvaX+Jt0pl91Ja+4PemkR7IUwKSwca/jU7wYBQh1N77AEh8B6Fd0mw5LoaoHMKvPcUt5kvUq/gRGg4QnLRFFNpggV44hmdb+QmbDrHRGrZyanfxi5Q1ZwDzxvgG2h/oMF9usUJ+FJ98NBcjMixTQRqqjAnbPqqRNrCrxjHbxGeK1O5szQxhG7tpDfWO3Dwcg7I8Q5agVSXwhv2LtSqWzoPiJv2TAFxaC4iFIZKwxs/SG1MvPqHlug=]]></Encrypt></xml>
+
+//不知道
+//<xml><ToUserName><![CDATA[toUser]]></ToUserName><Encrypt><![CDATA[NAI0nmXjKYE0jmiWpUD7eAHD3tk6gKR46qaF5UkkIX8XOHZCt8puONYG4OusSkZGXt02nQdDmhqMgYectezIGKoWUmIKEYXwOGhNmPB08n89U6SBXseHQwAaJpavmBult6ChGOpjVLHkpRFttu5VmrqNbT07SbgHRDfJUGVB3gDbTw3U8dphmOuPMLSthBDzW96BcxbKcCVYJ6gMeG+KVKXcel0y2ejtYGjMeIBryFeBlcoQrMtrcFTXY3R4uChlKAJVmDHgsf34Vwau7cz6eFsEsBiGz/xT5Ua6lk/gEsT+YCBvdgRV8Hj6zPOPVV4g5DES0mn6wYkwwwjTO0tUhZV3IkN6s3yNXG0cxRMV9umOhBjzSPot7zq6zIpqsI9w]]></Encrypt></xml>
